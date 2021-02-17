a7140729d4d8ebcc2629fccf400f9831faff8415
Guillem Delas <guillemdelas@hotmail.com>
2021-02-17 11:15:54 +0100

    updated test suits

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,13 +1,14 @@
 import { shallow } from 'enzyme';
 import App from '../javascript/App';
 
-describe('Pruebas en App', () => {
+describe('Testing the <App /> component', () => {
   const wrapper = shallow(<App />);
-  test('debe mostrarse correctamente', () => {
+
+  test('should render correctly', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  test('debe de contener el saludo Hello World!', () => {
+  test('should contain the greeting message Hello World!', () => {
     expect(wrapper.find('h1').exists()).toBe(true);
     expect(wrapper.find('h1').text().trim()).toBe('Hello World!');
   });
