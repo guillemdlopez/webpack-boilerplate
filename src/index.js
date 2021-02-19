17b501a4974a36da8f7f4f27432345d6338687b2
Guillem Delas <guillemdelas@hotmail.com>
2021-02-19 17:38:14 +0100

    removed useless imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import { render } from 'react-dom';
 import App from './javascript/App';
-import { nameless } from './javascript/function';
 
 import './style/stylesheets/index.scss';
 
 render(<App />, document.getElementById('root'));
-nameless();
