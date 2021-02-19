0fd28aa036f02ee7b40aeedce64b12c57eb73ae0
Guillem Delas <guillemdelas@hotmail.com>
2021-02-19 11:46:01 +0100

    fixed issue with hot reloading. Specified list of defaults browsers inside the babel config file insetad of creating the file separately

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,10 @@ if (process.env.NODE_ENV !== 'production') {
 
 module.exports = {
   presets: [
-    ['@babel/preset-env', { debug: true, useBuiltIns: 'usage', corejs: '3.9' }],
+    [
+      '@babel/preset-env',
+      { debug: true, useBuiltIns: 'usage', corejs: '3.9', targets: 'defaults' },
+    ],
     ['@babel/preset-react', { runtime: 'automatic' }],
   ],
   plugins,
