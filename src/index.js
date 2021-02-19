b71502a86dab12fe4a19a00e5df5ae24787bcec7
Guillem Delas <guillemdelas@hotmail.com>
2021-02-19 12:31:10 +0100

    removed testing js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
 import './style/stylesheets/index.scss';
 import { render } from 'react-dom';
 import App from './javascript/App';
-import { initTimer } from './javascript/timer';
 
 render(<App />, document.getElementById('root'));
-
-initTimer();
-
-const promise = function () {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('resolved!');
-    }, 5000);
-  });
-};
-
-const callingPromise = async function () {
-  console.log('calling');
-  const result = await promise();
-  console.log(result);
-};
-
-callingPromise();
