1749069b63aae9cbe6a36bfd8eead0534ad9e532
Guillem Delas <guillemdelas@hotmail.com>
2021-02-19 17:02:55 +0100

    added files that eslint should ignore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style/stylesheets/index.scss';
 import { render } from 'react-dom';
 import App from './javascript/App';
+import { nameless } from './javascript/function';
 
 render(<App />, document.getElementById('root'));
+nameless();
