c607794da74d988f725722fb648ed46c13bba7da
Guillem Delas <guillemdelas@hotmail.com>
2021-02-19 17:19:39 +0100

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style/stylesheets/index.scss';
+import React from 'react';
 import { render } from 'react-dom';
 import App from './javascript/App';
 import { nameless } from './javascript/function';
