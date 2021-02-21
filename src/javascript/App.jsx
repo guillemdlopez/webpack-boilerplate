4b725e3dbf169c4547c3fb2c4079220452574124
Guillem Delas <guillemdelas@hotmail.com>
2021-02-21 22:17:38 +0100

    removed smiley div

--- src/javascript/App.jsx
+++ src/javascript/App.jsx
@@ -4,9 +4,7 @@ const App = () => {
   return (
     <div className="container">
       <h1 style={{ textAlign: 'center' }}>Hello World!</h1>
-      <div id="smiley">
-        <img src={smiley} id="smiley-img" alt="smiley" />
-      </div>
+      <img src={smiley} id="smiley-img" alt="smiley" />
     </div>
   );
 };
