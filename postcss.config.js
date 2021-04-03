6e52830b94a5a9678b26cd0782ede8b615db33be
Guillem Delas <guillemdelas@hotmail.com>
2021-04-03 13:40:07 +0200

    included some postcss plugins and stylelint to lint scss and css stylesheets

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,18 @@
 module.exports = {
+  syntax: 'postcss-scss',
+  parser: 'postcss-safe-parser',
   plugins: {
     'postcss-import': true,
-    'postcss-preset-env': true,
+    'postcss-preset-env': {
+      stage: 0,
+    },
+    'postcss-nested': true,
     'postcss-responsive-type': true,
+    'postcss-discard-comments': true,
+    '@fullhuman/postcss-purgecss': {
+      content: ['./**/*.html'],
+      css: ['./src/stylesheets/*.*css'],
+    },
     autoprefixer: true,
   },
 };
